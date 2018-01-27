c3b6054bd051e02f89d4f851942aab24f403d42c
hughfenghen <hughfenghen@gmail.com>
2018-01-27 22:07:24 +0800

    支持跨域

--- devtools/mock.js
+++ devtools/mock.js
@@ -2,6 +2,8 @@ const path = require("path")
 const Koa = require("koa")
 const Router = require("koa-router")
 const glob = require("glob")
+const cors = require('@koa/cors')
+
 const app = new Koa()
 const router = new Router()
 
@@ -13,7 +15,7 @@ function sleep(time) {
   })
 }
 
-glob("./src/**/__apimocks__/*.js", (err, files) => {
+glob("./src/**/__apimocks__/*.js", async (err, files) => {
   if (err) throw err
   files.forEach(p => {
     const mock = require(path.resolve(process.cwd(), p))
@@ -27,6 +29,7 @@ glob("./src/**/__apimocks__/*.js", (err, files) => {
   })
 })
 
+app.use(cors({ credentials: true }))
 app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(3000)
