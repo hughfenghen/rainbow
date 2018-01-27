8c6190e3bc21b418a155a184e4aed194cacfac01
hughfenghen <hughfenghen@gmail.com>
2018-01-27 18:27:46 +0800

    devtools-mock

---
+++ devtools/mock.js
@@ -0,0 +1,32 @@
+const path = require("path")
+const Koa = require("koa")
+const Router = require("koa-router")
+const glob = require("glob")
+const app = new Koa()
+const router = new Router()
+
+function sleep(time) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve()
+    }, time)
+  })
+}
+
+glob("./src/**/__apimocks__/*.js", (err, files) => {
+  if (err) throw err
+  files.forEach(p => {
+    const mock = require(path.resolve(process.cwd(), p))
+    router[mock.method || "all"](mock.path, async (ctx, next) => {
+      ctx.body = mock.response
+
+      if (mock.delay) {
+        await sleep(mock.delay)
+      }
+    })
+  })
+})
+
+app.use(router.routes()).use(router.allowedMethods())
+
+app.listen(3000)
---
+++ src/rainbow/pages/__apimocks__/teams.js
@@ -0,0 +1,10 @@
+module.exports = {
+  path: '/api/team',
+  method: 'get',
+  delay: 2000,
+  response: {
+    code: 200,
+    msg: '',
+    data: {},
+  }
+}
