468425a3b3ec56c7a17bf40b4d11aaa97f035505
hughfenghen <hughfenghen@gmail.com>
2018-01-27 22:24:12 +0800

    fetch data

--- src/rainbow/pages/__apimocks__/teams.js
+++ src/rainbow/pages/__apimocks__/teams.js
@@ -1,10 +1,9 @@
 module.exports = {
-  path: '/api/team',
-  method: 'get',
-  delay: 2000,
+  path: "/api/team",
+  method: "get",
   response: {
     code: 200,
-    msg: '',
-    data: {},
+    msg: "",
+    data: [{ name: "项目一", progress: 80 }, { name: "项目二", progress: 20 }]
   }
 }
--- src/rainbow/pages/team.cljs
+++ src/rainbow/pages/team.cljs
@@ -1,21 +1,29 @@
 (ns rainbow.pages.team
-  (:require [rainbow.components.header :refer [header]])
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [rainbow.components.header :refer [header]]
+            [reagent.core :as reagent :refer [atom]]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (defonce projects (atom [{:name "项目一" :progress 90}
                          {:name "项目二" :progress 20}]))
 
 (defn team-view []
-  [:div
-   [header "团队视图"]
-   [:section
-    [:h4 {:style {:padding "0 3px"}} "前端研发组"]
-    (for [prj @projects
-          :let [name (:name prj)
-                progress (:progress prj)
-                key name]]
-      ^{:key key}
-      [prj-bar name progress])]])
+  (println "main:  component-DID-mount" ^:abc #{:a :b :c})
+  (go (let [response (<! (http/get "http://localhost:3000/api/team"))]
+    (println (-> response :body :data))
+    (reset! projects (-> response :body :data))))
+  (fn []
+    [:div
+     [header "团队视图"]
+     [:section
+      [:h4 {:style {:padding "0 3px"}} "前端研发组"]
+      (for [prj @projects
+            :let [name (:name prj)
+                  progress (:progress prj)
+                  key name]]
+        ^{:key key}
+        [prj-bar name progress])]]))
 
 (defn prj-bar [name progress]
   [:div {:style {:border-width "1px 0"
