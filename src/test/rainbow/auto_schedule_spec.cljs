fc26a2aaccc6028f41a168aaa2e2bb5f4de6a5e0
hughfenghen <hughfenghen@gmail.com>
2018-07-15 15:41:01 +0800

    测试用例

--- src/test/rainbow/auto_schedule_spec.cljs
+++ src/test/rainbow/auto_schedule_spec.cljs
@@ -1,30 +1,31 @@
 (ns test.rainbow.auto-schedule-spec
   (:require [cljs-time.core :as t]
+            [cljs-time.coerce :refer [to-date]]
             [cljs.test :refer (deftest is)]))
 
 (def data {:prjName "项目一"
            :startTime #inst "2018-03-03"
            :endTime #inst "2018-06-03"
-           :resource [{:id "r1" :name "zhangsan" :task ["t1"]}
+           :resource [{:id "r1" :name "zhangsan" :task ["t1" "t4"]}
                       {:id "r2" :name "lisi" :task ["t2" "t3"]}]
-           :prjTask [{:id "t1" :name "task1" :workload 24 :startTime #inst "2018-06-01"}
+           :prjTask [{:id "t1" :name "task1" :workload 24}
                      {:id "t2" :name "task2" :workload 4}
                      {:id "t4" :name "task4" :workload 12}]})
-(js/console.log 111
-                (t/in-days (t/days 9)))
 
 (defn create-alloc [s-date e-date]
   (let [limit (t/in-days (t/interval s-date e-date))
         balance (atom limit)
-        cursor (atom s-date)]
+        cursor (atom (t/date-time s-date))]
 
     (fn [bill]
       (if (>= @balance bill)
-        (do (reset! balance (- @balance bill))
-            (reset! cursor (t/plus @cursor
-                                   (t/days bill)))
-            true)
-        false))))
+        (let [cur-start-date @cursor
+              end-date (t/plus (t/date-time @cursor)
+                               (t/days bill))]
+          (reset! balance (- @balance bill))
+          (reset! cursor end-date)
+          (map to-date [cur-start-date end-date]))
+        nil))))
 
 (defn ttt [data]
   (let [allocated-taskid (->> data
@@ -37,11 +38,16 @@
         allocated-task (->> data
                             (:prjTask)
                             (filter #(contains? allocated-taskid
-                                                (:id %))))]
-    allocated-task))
+                                                (:id %))))
+        alloc (create-alloc prj-startTime prj-endTime)]
+
+    (map #(if-let [plan (alloc (:workload %))]
+            (assoc % :plan plan)
+            %)
+         allocated-task)))
 
 (deftest auto-schedule
-  (is (= (ttt data) 1)))
+  (is (= (ttt data) [#inst "2018-03-03" #inst "2018-03-13"])))
 
 (deftest a-failing-test
   (is (= 1 1)))
